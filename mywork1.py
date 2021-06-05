675607f2be7530eefba2e11b5a96881a8e0a7ce8
robinraj1999 <68849138+robinraj1999@users.noreply.github.com>
2021-06-05 16:49:47 +0400

    Update mywork1.py

--- mywork1.py
+++ mywork1.py
@@ -33,7 +33,6 @@ with header:
         <img src="https://media.tenor.com/images/f6378d5c837d255a894418a2f38d3a01/tenor.gif" width="600" />
     </a>''',
     unsafe_allow_html=True
-    st.title('Final Year Project')
 )
     
     st.title('Final Year Project')
